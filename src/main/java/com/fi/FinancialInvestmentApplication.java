1356e09bc4e27a9d08b2222bb19b9cd8b558b1eb
Changjun Park <cjpark@hanamicron.co.kr>
2021-07-02 10:30:00 +0900

    프로젝트 생성

---
+++ src/main/java/com/fi/FinancialInvestmentApplication.java
@@ -0,0 +1,13 @@
+package com.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialInvestmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinancialInvestmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fi/FinancialInvestmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialInvestmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
